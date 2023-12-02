221069b05f519de1e7a1933ed90a80eea3a31421
EslamSad3 <eslam.sa3d96@gmail.com>
2023-12-02 04:04:17 +0200

    ProtectedRoute

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,9 +1,7 @@
 import React, { useContext } from "react";
-import { Context } from "../../context";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute(props) {
-  const { adminToken } = useContext(Context);
   if (!localStorage.getItem("AdminToken")) {
     return <Navigate to={"/login"} />;
   } else {
